5e73f5b4ba001c861ce363a8e04399436626a762
Rydlis <david.rejdl@gmail.com>
2015-05-09 19:02:15 +0200

    Update, rozdělané GUI na nastavení testování

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,18 +7,46 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 
-public class Controller {
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
     // zavdeni všeho potřebného
     @FXML
     private ListView<String> listView;
+    @FXML
+    private GridPane nastaveni_div;
+    @FXML
+    private GridPane zkouseni_div;
+    @FXML
+    private GridPane tlacitka_div;
+    @FXML
+    private ImageView ceska_vlajka;
+    @FXML
+    private ImageView anglicka_vlajka;
 
     private ObservableList<String> list = FXCollections.observableArrayList();
     int i = 0;
 
     Slovicka slovicka = new Slovicka();
     Dialogy dialogy = new Dialogy();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ceska_vlajka.setImage(new Image(new File("czech.jpg").toURI().toString()));
+        anglicka_vlajka.setImage(new Image(new File("uk.jpg").toURI().toString()));
+        zkouseni_div.setOpacity(0);
+        zkouseni_div.setDisable(true);
+        tlacitka_div.setOpacity(0);
+        tlacitka_div.setDisable(true);
+    }
 
     // ne zcela pekna funkce na import slovicek a jejich vykresleni do ListView
     public void handleImport(ActionEvent event) {
@@ -38,12 +66,16 @@ public class Controller {
     }
 
     public void handleStartTest(){
-
+        nastaveni_div.setDisable(true);
+        nastaveni_div.setOpacity(0);
+        zkouseni_div.setOpacity(100);
+        zkouseni_div.setDisable(false);
+        tlacitka_div.setOpacity(100);
+        tlacitka_div.setDisable(false);
     }
 
     // funkce pro ukončení aplikace
     public void handleExit (){
         System.exit(0);
     }
-
 }
